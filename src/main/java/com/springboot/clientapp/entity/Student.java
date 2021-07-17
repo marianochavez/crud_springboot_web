aac80b536b5069b0ba78e508f4fa437417aa995a
marianochavez <amarianochavez@gmail.com>
2021-07-17 17:39:57 -0300

    added attrib student - fix html

--- src/main/java/com/springboot/clientapp/entity/Student.java
+++ src/main/java/com/springboot/clientapp/entity/Student.java
@@ -18,45 +18,94 @@ public class Student {
 	@Column(name = "name")
 	private String name;
 	
+	@Column(name = "last_name")
+	private String lastName;
+	
 	@Column(name = "email")
 	private String email;
 	
 	@Column(name = "phone_no")
 	private long phoneNo;
 	
+	@Column(name = "course")
+	private String course;
+	
 	
 	public Student() {
 		super();
 	}
 
-	public Student(String name, String email) {
+	
+	public Student(String name, String lastName, String email, long phoneNo, String course) {
 		super();
 		this.name = name;
+		this.lastName = lastName;
 		this.email = email;
+		this.phoneNo = phoneNo;
+		this.course = course;
 	}
+
+
+	
 	
 	public long getId() {
 		return id;
 	}
+
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
+
 	public String getName() {
 		return name;
 	}
+
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
 	public String getEmail() {
 		return email;
 	}
+
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+
 	public long getPhoneNo() {
 		return phoneNo;
 	}
+
+
 	public void setPhoneNo(long phoneNo) {
 		this.phoneNo = phoneNo;
 	}
+
+
+	public String getCourse() {
+		return course;
+	}
+
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	
+	
 }
