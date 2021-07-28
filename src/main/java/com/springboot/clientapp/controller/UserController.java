33c5d8f916e95f7f2088c2b7395b4f68fc620200
marianochavez <amarianochavez@gmail.com>
2021-07-28 12:01:06 -0300

    fix templates - pagination

--- src/main/java/com/springboot/clientapp/controller/UserController.java
+++ src/main/java/com/springboot/clientapp/controller/UserController.java
@@ -1,14 +1,21 @@
 package com.springboot.clientapp.controller;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.springboot.clientapp.entity.User;
 import com.springboot.clientapp.repository.UserRepository;
@@ -21,9 +28,26 @@ public class UserController {
 	
 	
 	@GetMapping("list")
-	public String listUsers(Model model) {
-	    List<User> listUsers = userRepository.findAll();
-	    model.addAttribute("listUsers", listUsers);
+	public String listUsers(Model model, @RequestParam("page") Optional<Integer> page) {
+	    
+		int currentPage = page.orElse(1);
+
+		int pageSize = 10;
+
+		Pageable paging = PageRequest.of(currentPage - 1, pageSize);
+
+		Page<User> userPage = this.userRepository.findAll(paging);
+		
+		int totalPages = userPage.getTotalPages();
+
+		model.addAttribute("users", userPage);
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("totalPages", totalPages);
+		
+		if (totalPages > 0) {
+			List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages).boxed().collect(Collectors.toList());
+			model.addAttribute("pageNumbers", pageNumbers);
+		}
 	     
 	    return "user/users";
 	}
